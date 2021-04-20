0bc9d7df8ed750ca8badb7196d5f6d9e730d4b12
Huy <huytdps13400@fpt.edu.vn>
2021-04-20 10:14:45 +0700

    master

---
+++ theme/index.js
@@ -0,0 +1,33 @@
+import {Platform} from 'react-native';
+
+export const theme = {
+  colors: {
+    text: '#242424',
+    background: '#f5f5f5',
+    orange: '#FE930F',
+    lightGray: '#A5A5A5',
+    gray: '#424242',
+    smoke: '#E6E6E6',
+    white: '#ffffff',
+    black: '#000000',
+    placeholder: '#707070',
+    blue: '#0d5cb6',
+    red: '#E83625',
+    gradient: ['#F04831', '#E73222', '#D9100C'],
+    green: '#088A08',
+    lightGreen: '#29bb89',
+    yellow: '#FFDF00',
+  },
+
+  fonts: {
+    fontWeight: {
+      bold: 'bold',
+      semibold: Platform.OS === 'android' ? 'bold' : '600',
+      regular: 'normal',
+      light: '300',
+    },
+    fontFamily: {
+      default: Platform.OS === 'ios' ? 'System' : 'Roboto',
+    },
+  },
+};

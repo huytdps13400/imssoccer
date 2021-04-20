310cab678ffe9a03be63bc44c57a376178253ca0
Huy <huytdps13400@fpt.edu.vn>
2021-04-20 10:44:23 +0700

    icon-font

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const icon = {
+  home: require('./icons/icon-home.png'),
+  profile: require('./icons/icon-users.png'),
+  notification: require('./icons/icon-bell.png'),
+  logout: require('./icons/iconlogout.png'),
+  registration: require('./icons/iconuserregistration.png'),
+  logoims: require('./icons/logoims.png'),
+  back: require('./icons/back.png'),
+  search: require('./icons/icon-search.png'),
+};
+export {icon};
